82d73670727afb1b63fbff5a0f6e761a380137c4
rchui <ryan.w.chui@gmail.com>
2017-03-11 01:50:56 -0600

    fixed thread safe regex

--- perg.cpp
+++ perg.cpp
@@ -83,19 +83,20 @@ void printSingle(std::queue<std::string> *filePaths, Settings *instance) {
 		}
 		#pragma omp parallel for
 		for (int i = 0; i < count; ++i) {
+			std::string line2;
 			#pragma omp critical
-			std::getline(file2, line);
+			std::getline(file2, line2);
 			if ((*instance).verbose) {
-				if (!std::regex_search(line.begin(), line.end(), rgx) && (*instance).invert) {
-					std::cout << (*filePaths).front() + ": " + line + "\n";
+				if (!std::regex_search(line2.begin(), line2.end(), rgx) && (*instance).invert) {
+					std::cout << (*filePaths).front() + ": " + line2 + "\n";
 				} else if (std::regex_search(line.begin(), line.end(), rgx) && !(*instance).invert) {
-					std::cout << (*filePaths).front() + ": " + line + "\n";
+					std::cout << (*filePaths).front() + ": " + line2 + "\n";
 				}
 			} else {
-				if (!std::regex_search(line.begin(), line.end(), rgx) && (*instance).invert) {
-					std::cout << line + "\n";
-				} else if (std::regex_search(line.begin(), line.end(), rgx) && !(*instance).invert) {
-					std::cout << line + "\n";
+				if (!std::regex_search(line2.begin(), line2.end(), rgx) && (*instance).invert) {
+					std::cout << line2 + "\n";
+				} else if (std::regex_search(line2.begin(), line2.end(), rgx) && !(*instance).invert) {
+					std::cout << line2 + "\n";
 				}
 			}
 		}

4dd3c9d17280714eeb5c9613f1f9209474adc53c
rchui <ryan.w.chui@gmail.com>
2017-03-10 18:51:23 -0600

    private line string

--- perg.cpp
+++ perg.cpp
@@ -82,8 +82,9 @@ void printSingle(std::queue<std::string> *filePaths, Settings *instance) {
 
 		#pragma omp parallel for schedule(static)
 		for (int i = 0; i < count; ++i) {
+			std::string line2;
 			#pragma omp critical
-			std::getline(file2, line);
+			std::getline(file2, line2);
 			if ((*instance).verbose) {
 				if (std::regex_search(line.begin(), line.end(), rgx) && (*instance).invert) {
 					std::cout << (*filePaths).front() + ": " + line + "\n";
